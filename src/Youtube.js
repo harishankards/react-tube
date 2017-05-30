1415adc12adc344e7f5e7ed92ff45f297c1866a8
harishankards <harishankards@gmail.com>
2017-05-30 16:49:33 +0530

    Add and initiate Youtube component

---
+++ src/Youtube.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Youtube extends Component {
+    render(){
+      return(
+        <div>
+            Pinging from Youtube!!!
+        </div>
+      );
+    }
+
+  }
+
+export default Youtube;
